75347cccd456914e73c2158768cfe560d7d65ba1
CamilinLopez <camilollc98@gmail.com>
2023-10-18 09:59:37 -0500

    finished footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import { ThreeItemGrid } from "@/components/grid/three-items";
+import { Suspense } from "react";
+import { Carousel } from "@/components/carousel";
+import Footer from "@/components/layout/footer";
 
 export default function Home() {
   return (
     <>
       <ThreeItemGrid />
+      <Suspense>
+        <Carousel />
+        <Suspense>
+          <Footer />
+        </Suspense>
+      </Suspense>
     </>
   );
 }
